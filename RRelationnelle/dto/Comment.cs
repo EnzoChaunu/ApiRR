7e46e742912e2ed2438389542476d9ffffa8c805
Chaunu Enzo <chaunu.enzo@hotmail.fr>
2023-01-19 14:33:36 +0100

    service

--- RRelationnelle/dto/Comment.cs
+++ RRelationnelle/dto/Comment.cs
@@ -1,21 +1,22 @@
 ﻿
 
 using System;
-
+// commmm
 namespace RessourcesRelationelles.Class
 {
     public class Comments
     {
-        private int _id;
-        private User _user;
-        private Ressource _ressource;
-        private string _content;
-        private int _likes;
-        private int _dislikes;
-        private bool _activation;
-        private bool _modified;
-        private DateTime _creationDate;
-        private DateTime _modificationDate;
+        public int _id;
+        public User _user;
+        public Ressource _ressource;
+        public string _content;
+        public int _likes;
+        public int _dislikes;
+        public bool _activation;
+        public bool _modified;
+        public DateTime _creationDate;
+        public
+            DateTime _modificationDate;
 
         public int Id { get { return _id; } }
         public string Content { get { return _content; } set { _content = value; } }
