cd17341b0978d78c422d0426dcd5c5547595a90b
GuigDo <115692509+GuigDo@users.noreply.github.com>
2023-02-13 12:16:55 +0100

    Modif Mapping de Roles

--- RRelationnelle/Mapping/MappingRoles.cs
+++ RRelationnelle/Mapping/MappingRoles.cs
@@ -1,6 +1,34 @@
-﻿namespace RRelationnelle.Mapping
+﻿using AutoMapper;
+using RessourcesRelationelles.Class;
+using RRelationnelle.Modèles;
+
+namespace RRelationnelle.Mapping
 {
-    public class MappingRoles
+    public class MappingRoles : Profile
     {
+        public static Mapper MappingRolesL()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Roles, Category>()
+
+               .ForMember(dest =>
+                   dest._name,
+                   opt => opt.MapFrom(src => src._name))
+              .ForMember(dest =>
+                    dest._creationDate, src => src.Ignore())
+                .ForMember(dest =>
+                    dest.Id_Category, src => src.Ignore())
+                .ForMember(dest =>
+                    dest._creator, opt => opt.MapFrom(src => src.idcreator))
+                .ReverseMap();
+
+            });
+            var mapper = new Mapper(config);
+            return mapper;
+
+
+
+        }
     }
 }
