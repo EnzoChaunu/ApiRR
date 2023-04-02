2a4c00f062d90ebef32c0fd3564de0c22f934db1
Chaunu Enzo <chaunu.enzo@hotmail.fr>
2023-04-02 18:15:22 +0200

    creation controller/service user

---
+++ Service/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRelationnelle.Services
+{
+    public class UserService : IUserService
+    {
+        public UserService()
+        {
+            
+        }
+        // toto
+        public Task<bool> Archive(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Create(UserDto obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Update(UserDto obj, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
